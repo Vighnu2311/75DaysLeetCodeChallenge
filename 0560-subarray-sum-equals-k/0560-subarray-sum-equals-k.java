3ff53a05e67d3390ec96089ef47870778102c3ac
Vighnu2311 <vighnapriyaandhavarapu@gmail.com>
2026-05-01 21:47:49 +0530

    Time: 26 ms (31.61%), Space: 48.8 MB (60.85%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -2,26 +2,19 @@ import java.util.HashMap;
 
 class Solution {
     public int subarraySum(int[] nums, int k) {
-            HashMap<Integer, Integer> map = new HashMap<>();
-                    
-                int sum = 0;
-           int count = 0;
-                                            
-             // Important: base case
-            map.put(0, 1);
-                                                                    
-             for (int num : nums) {
-                sum += num;
-                                                                                                    
-                                                                                                                // If (sum - k) exists, add its frequency
-                                                                                                                            if (map.containsKey(sum - k)) {
-                                                                                                                                            count += map.get(sum - k);
-                                                                                                                                                        }
-                                                                                                                                                                    
-                                                                                                                                                                                // Store current sum frequency
-                                                                                                                                                                                            map.put(sum, map.getOrDefault(sum, 0) + 1);
-                                                                                                                                                                                                    }
-                                                                                                                                                                                                            
-                                                                                                                                                                                                                    return count;
-                                                                                                                                                                                                                        }
-                                                                                                                                                                                       }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+
+        int sum = 0, count = 0;
+
+        for (int num : nums) {
+            sum += num;
+
+            count += map.getOrDefault(sum - k, 0);
+
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+
+        return count;
+    }
+}
